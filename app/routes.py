13a5961848184a19e0985d4c30357206f4a53add
Jessica <Yiskah.Scientia@gmail.com>
2023-04-22 16:35:25 -0600

    Second endpoint about kittens

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,11 @@ hello_world_bp = Blueprint("hello_world", __name__)
 def say_hello_world():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body, 200
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name" : "Calico",
+        "message" : "She's a little kitten.",
+        "personality" : ["Naughty", "Nice", "Curious"]
+    }, 200
