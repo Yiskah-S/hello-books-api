c7c96ede4ba6449fe5287bd68fb650da4d199b08
Jessica <Yiskah.Scientia@gmail.com>
2023-04-22 18:12:01 -0600

    Fourth Endpoint - books

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title A", "A fantasy novel set in an abc imaginary world."),
+    Book(2, "Fictional Book Title B", "A fantasy novel set in an xyz imaginary world."),
+    Book(3, "Fictional Book Title B", "A fantasy novel set in an 123 imaginary world.")
+] 
 
 hello_world_bp = Blueprint("hello_world", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response), 200
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
-
 def say_hello_world():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body, 200
