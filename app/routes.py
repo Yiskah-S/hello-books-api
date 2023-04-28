ae19213bdb8cd9427851918a2c1e3e94d9b773c8
Jessica <Yiskah.Scientia@gmail.com>
2023-04-28 14:24:44 -0600

    GET & POST

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,49 @@ from flask import Blueprint, jsonify, make_response, request
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_books():
     request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
-
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        )
+    
     db.session.add(new_book)
     db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+    message = f"Book {new_book.title} succesfully created"
+    return make_response(message, 201)
+    
+@books_bp.route("", methods=["GET"])
+def get_all_books():
+    books = Book.query.all()
+    book_result = []
+    for book in books:
+        book_result.append(
+            dict(
+            id=book.id,
+            title=book.title,
+            description=book.description
+            )
+        )
+    return jsonify(book_result)
+
+# from app import db
+# from app.models.book import Book
+# from flask import Blueprint, jsonify, make_response, request
+
+# books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+# @books_bp.route("", methods=["POST"])
+# def handle_books():
+#     request_body = request.get_json()
+#     new_book = Book(title=request_body["title"],
+#                     description=request_body["description"])
+
+#     db.session.add(new_book)
+#     db.session.commit()
+
+#     return make_response(f"Book {new_book.title} successfully created", 201)
 
 
 
